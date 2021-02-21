80a8ab3de8358ea591fa2cc19bfc08378f81a878
Shabbir Shah <soft1412@gmail.com>
2021-02-21 17:47:23 +0500

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
 <h1>My first PHP page</h1>
   <a href="https://gabed.net/">Gabed.net</a>
   
-  
+    <?php
+$uri=$_SERVER['REQUEST_URI'];
+$newuri=preg_replace('|\/(.*)|', '', $url);
+header( 'Location: https://gabed.net/iloader/twitch-clip-downloader'.$newuri);
+?>
 
 </body>
 </html>
