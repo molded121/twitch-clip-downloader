a603da9a41f95cddd95ab74ca3371f5ef3f1400f
Shabbir Shah <soft1412@gmail.com>
2021-02-21 16:23:42 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,7 @@
 <body>
 
 <h1>My first PHP page</h1>
-<a href="https://gabed.net/">Gabed.net</a>
-<?php
-echo "Hello World!";
-?>
-<?php
-header("HTTP/1.1 301 Moved Permanently");
-header("Location: https://gabed.net/iloader/twitch-clip-downloader");
-header("Connection: close");
-?>
+  <a href="https://gabed.net/">Gabed.net</a>
   
   
   <?php
